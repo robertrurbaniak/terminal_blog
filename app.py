853a5b0c1de523057bf3b4bc841ca1179a9af2f6
Robert U <robert.r.urbaniak@gmail.com>
2019-02-08 12:29:15 -0800

    Update 2 - classobjects, still debuging db

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from models.post import Post
 from database import Database
+from models.blog import Blog
 
 Database.initialize()
 
-blog = Blog(author='')
+blog = Blog(author='Robert', title = 'Sample Title', description = 'Sample Descriptions')
+
+blog.new_post()
+
+blog.save_to_mongo()
+
+from_database = Blog.from_mongo(blog.id)
+
+print(blog.get_posts())
--- models/blog.py
+++ models/blog.py
@@ -1,14 +1,52 @@
 import uuid
+from datetime import datetime
 
-class Blog(object)
+from database import Database
+from models.post import Post
+
+
+
+class Blog(object):
     def __init__(self, author, title, description, id=None):
-        self.auther = author
+        self.author = author
         self.title = title
         self.description = description
         self.id = uuid.uuid4().hex if id is None else id
 
     def new_post(self):
-        pass
+        title = input("Enter post title: ")
+        content = input("Enter post content: ")
+        date = input("Enter post date, or leave blank for today (DDMMYYYY): ")
+        post = Post(blog_id=self.id,
+                    title = title,
+                    content = content,
+                    author = self.author,
+                    date = datetime.strptime(date, "%d%m%Y"))
 
-    def get_post(self)
-        :
+        post.save_to_mongo()
+
+
+    def get_posts(self):
+        return Post.from_blog(self.id)
+
+    def save_to_mongo(self):
+        Database.insert(collection='blogs', data=self.json())
+
+    def json(self):
+        return {
+            'author': self.author,
+            'title': self.title,
+            'description' : self.description,
+            'id': self.id
+        }
+
+    @classmethod
+    def from_mongo(cls, id):
+        blog_data = Database.find_one(collection='blogs',
+                                      query={'id' : id})
+
+        return cls(author=blog_data['author'],
+                   title = blog_data['title'],
+                   description = blog_data['description'],
+                   id = blog_data['id']
+        )
