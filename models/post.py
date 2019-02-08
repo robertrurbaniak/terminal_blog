06d946f7f5e26747bc5143fa0c2fa7d3e4fd73d7
Robert U <robert.r.urbaniak@gmail.com>
2019-02-08 12:26:22 -0800

    Update 2 - classobjects, still debuging db

--- models/post.py
+++ models/post.py
@@ -1,10 +1,10 @@
 import uuid
 from database import Database
-import datetime
+from datetime import datetime
 
 class Post(object):
 
-    def __init__(self, blog_id, title, content, author, date=datetime.datetime.utcnow(), id=None):
+    def __init__(self, blog_id, title, content, author, date=datetime.utcnow(), id=None):
         self.blog_id = blog_id
         self.title = title
         self.content = content
@@ -12,11 +12,8 @@ class Post(object):
         self.created_date = date
         self.id = uuid.uuid4().hex if id is None else id
 
-
-
     def save_to_mongo(self):
-        Database.insert(collection='posts',
-                        data=self.json())
+        Database.insert(collection='posts', data=self.json())
 
     def json(self):
         return {
@@ -28,10 +25,18 @@ class Post(object):
             'created_date': self.created_date
         }
 
-    @staticmethod
-    def from_mongo(id):
+    @classmethod
+    def from_mongo(cls, id):
         # Ex: Post.from_mongo('123') returns a post with that id
-        return Database.find_one(collection='posts', query={'id':id})
+        post_data = Database.find_one(collection='posts', query={'id':id})
+
+        return cls(blog_id=post_data['blog_id'],
+                   title=post_data['title'],
+                   content=post_data['content'],
+                   author=post_data['author'],
+                   date=post_data['created_date'],
+                   id=post_data['id']
+                   )
 
     @staticmethod
     def from_blog(id):
